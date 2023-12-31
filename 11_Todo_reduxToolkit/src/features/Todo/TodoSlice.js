ed4d7a385fb513f9e9b0530d3f866517daea82cd
pradeepadlakondawar <94275183+pradeepadlakondawar@users.noreply.github.com>
2023-12-31 11:56:16 +0000

    Proj_11_Todo_reduxToolkit

--- 11_Todo_reduxToolkit/src/features/Todo/TodoSlice.js
+++ 11_Todo_reduxToolkit/src/features/Todo/TodoSlice.js
@@ -16,7 +16,7 @@ export const todoSlice = createSlice({
                 id : nanoid(),
                 text : action.payload
             }
-            state.todos.push
+            state.todos.push(todo)
         },
         removeTodo : (state, action) => {
             state.todos = state.todos.filter((todo)=>todo.id !== action.payload)
